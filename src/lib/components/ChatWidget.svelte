285be16bcdf85af44e396d6f676c1ec86311ee7c
Luka <luka@studio42.art>
2026-04-18 11:32:25 +0200

    feat: add Tawk.to Widget ID (69e34e81a7ff261c327a9efc) - chat now live

--- src/lib/components/ChatWidget.svelte
+++ src/lib/components/ChatWidget.svelte
@@ -9,7 +9,7 @@
 			var s1 = document.createElement('script'),
 				s0 = document.getElementsByTagName('script')[0];
 			s1.async = true;
-			s1.src = 'https://embed.tawk.to/YOUR_TAWK_ID/1234567890'; // Replace YOUR_TAWK_ID and numbers
+			s1.src = 'https://embed.tawk.to/69e34e81a7ff261c327a9efc/1234567890';
 			s1.charset = 'UTF-8';
 			s1.setAttribute('crossorigin', '*');
 			s0.parentNode.insertBefore(s1, s0);
